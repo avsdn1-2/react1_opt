b529d33413c4e316e3fd6d95333288d267acdf42
Andrey <vsirik20gmail.com>
2021-05-21 17:54:57 +0300

    final version

--- src/block.js
+++ src/block.js
@@ -3,9 +3,8 @@ import './MyApp.css';
 
 class Block extends React.Component {
     render() {
-
         return (
-            <div style={{width:'500px',border:'2px solid magenta',margin:'0 auto 10px auto'}}>
+            <div key={ this.props.item.id } style={{width:'500px',border:'2px solid magenta',margin:'0 auto 10px auto'}}>
 
                 {this.props.item.isSpecial && <div style={{color: 'red'}}>Special</div> }
                 {this.props.item.link == null? <div><span className="bold_w">Заголовок:</span> {this.props.item.title}</div> : <div>Заголовок: <a href={this.props.item.link}>{this.props.item.title}</a></div>}
@@ -15,13 +14,10 @@ class Block extends React.Component {
                 <div className="bold_w">Краткое содержание</div>
                 <div dangerouslySetInnerHTML={{__html: this.props.item.content}} />
 
-
-
-
                 { this.props.item.categories.length > 0 && <div className="bold_w">Категории</div> }
-                {  this.props.item.categories.length > 0 &&  this.props.item.categories.map((category,i) =>
-                    <p>{category.name}</p>
-                )
+                {
+                    this.props.item.categories.length > 0 &&  this.props.item.categories.map((category,i) =>
+                    <p>{category.name}</p>)
                 }
                 { this.props.item.photo !== null && <img src={this.props.item.photo}/>}
             </div>
